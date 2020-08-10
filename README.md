cdcad6959ec3e84aca1375133d10cc6abb9e0a10
Rianna Suen <vividfax@gmail.com>
2020-08-10 23:41:44 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Colourway
+
+A tool that shows the closest matching yarns for a specific colour from a colour picker.
--- sketch.js
+++ sketch.js
@@ -1,18 +1,42 @@
+let data;
+
+let cache;
+
 let colorPicker;
 let hexCode;
+let rgb;
+let options;
+
+function preload() {
+	data = loadJSON("./data.json");
+}
 
 function setup() {
 
-	colorPicker = createColorPicker("#35BC95");
+	colorPicker = createColorPicker("#ffcaa9");
 	colorPicker.parent("#pickerContainer");
 	colorPicker.size(200, 200);
 
 	hexCode = createP(colorPicker.value());
 	hexCode.parent("#textContainer");
 	hexCode.style("text-transform", "uppercase");
+
+	rgb = createP(colorPicker.color());
+	rgb.parent("#textContainer");
+
+	options = createP("West Yorkshire Spinners ColourLab DK - Deep Teal 716");
+	options.parent("#optionsContainer");
+
+	cache = colorPicker.value();
 }
 
 function draw() {
 
+	if (cache == colorPicker.value()) {
+		return;
+	}
+	cache = colorPicker;
+
 	hexCode.html(colorPicker.value());
+	rgb.html(colorPicker.color());
 }
