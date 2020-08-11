0960fb95030712650756a0d3406de4be408391a6
Rianna Suen <vividfax@gmail.com>
2020-08-11 15:06:13 +0100

    List options

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function preload() {
 
 function setup() {
 
-	colorPicker = createColorPicker("#ffcaa9");
+	colorPicker = createColorPicker("#ff744c");
 	colorPicker.parent("#pickerContainer");
 	colorPicker.size(200, 200);
 
@@ -24,8 +24,7 @@ function setup() {
 	rgb = createP(colorPicker.color());
 	rgb.parent("#textContainer");
 
-	options = createP("West Yorkshire Spinners ColourLab DK - Deep Teal 716");
-	options.parent("#optionsContainer");
+	printData();
 
 	cache = colorPicker.value();
 }
@@ -35,8 +34,42 @@ function draw() {
 	if (cache == colorPicker.value()) {
 		return;
 	}
-	cache = colorPicker;
+	cache = colorPicker.value();
 
 	hexCode.html(colorPicker.value());
 	rgb.html(colorPicker.color());
+}
+
+function printData() {
+
+	let size = data.yarns.length;
+	console.log(data.yarns.length)
+
+	for (let i = 0; i < size; i++) {
+		printYarn(i);
+	}
+}
+
+function printYarn(i) {
+
+	let size = data.yarns[i].colorways.length;
+
+	for (let j = 0; j < size; j++) {
+		printColorway(i, j);
+	}
+}
+
+function printColorway(i, j) {
+
+	let colourway = data.yarns[i].colorways[j].name;
+	let yarn = data.yarns[i].name;
+	let toPrint = [yarn, colourway].join(" - ");
+	let link = data.yarns[i].url;
+
+	let optionsHolder = createP();
+	optionsHolder.parent("#optionsContainer");
+	optionsHolder.style("background-color", data.yarns[i].colorways[j].hex)
+	options = createA(link, toPrint);
+	options.parent(optionsHolder);
+	options.style("color", "#eee")
 }
