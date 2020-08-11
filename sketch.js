0a82df51d55de90eb219679c7b5891761889afdf
Rianna Suen <vividfax@gmail.com>
2020-08-11 17:26:02 +0100

    Add rowan yarn

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function preload() {
 
 function setup() {
 
-	colorPicker = createColorPicker("#b1be70");
+	colorPicker = createColorPicker("#be9092");
 	colorPicker.parent("#pickerContainer");
 	colorPicker.size(200, 200);
 
@@ -40,38 +40,6 @@ function draw() {
 	rgb.html(colorPicker.color());
 
 	getClosest(colorPicker.value());
-}
-
-function printColorway(i, j) {
-
-	let colourway = data.yarns[i].colorways[j].name;
-	let yarn = data.yarns[i].name;
-	let toPrint = [yarn, colourway].join(" - ");
-	let link = data.yarns[i].url;
-
-	let optionsHolder = createP();
-	optionsHolder.parent("#optionsContainer");
-	optionsHolder.style("background-color", data.yarns[i].colorways[j].hex)
-	options = createA(link, toPrint);
-	options.parent(optionsHolder);
-	options.style("color", "#eee")
-}
-
-function colorDistance(first, second) {
-
-	let r = makeEven(red(first) - red(second));
-	let g = makeEven(green(first) - green(second));
-	let b = makeEven(blue(first) - blue(second));
-
-	return r + g + b;
-}
-
-function makeEven(n) {
-
-	if (n < 0) {
-		return n * -1;
-	}
-	return n;
 }
 
 function getClosest(original) {
@@ -108,7 +76,41 @@ function getClosest(original) {
 		}
 	}
 	document.getElementById("optionsContainer").innerHTML = "";
+
 	printColorway(closestData[0], closestData[1]);
 	printColorway(secondData[0], secondData[1]);
 	printColorway(thirdData[0], thirdData[1]);
+}
+
+function colorDistance(first, second) {
+
+	let r = makeEven(red(first) - red(second));
+	let g = makeEven(green(first) - green(second));
+	let b = makeEven(blue(first) - blue(second));
+
+	return r + g + b;
+}
+
+function makeEven(n) {
+
+	if (n < 0) {
+		return n * -1;
+	}
+	return n;
+}
+
+function printColorway(i, j) {
+
+	let colourway = data.yarns[i].colorways[j].name;
+	let yarn = data.yarns[i].name;
+	let toPrint = [yarn, colourway].join(" - ");
+	let link = data.yarns[i].url;
+
+	let optionsHolder = createP();
+	optionsHolder.parent("#optionsContainer");
+	optionsHolder.style("background-color", data.yarns[i].colorways[j].hex);
+	options = createA(link, toPrint);
+	options.parent(optionsHolder);
+	options.style("color", "#eee");
+	options.style("padding", "8px");
 }
